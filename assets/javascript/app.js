28d697b29817ac3bb076fad405d485edf932adc1
Will Hathaway <william.hathaway38@gmail.com>
2019-11-05 09:43:40 -0500

    added code/js file for d3 word cloud

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,9 +2,7 @@
 
 let APIkey = 'a93e9eaa48f8479c927c31d2e61713c0';
 
-// global variables:
-
-// empty arrays to be filled by ajax request:
+// an array of objects to hold the information for constructing the query:
 
 let sourceArray = [
     fox = {
@@ -34,28 +32,53 @@ let sourceArray = [
     }
 ];
 
-$(document).ready(function(){
+// dynamically adds the sources to two dropdown menus (NOT WORKING):
 
-for (let i = 0; i < sourceArray.length; i++) {
+$(document).ready(function () {
 
-    $('#dropdown-menu-left').append('<a class="dropdown-item">').val(sourceArray[i].url).text(sourceArray[i].name);
+    for (let i = 0; i < sourceArray.length; i++) {
 
-    $('#dropdown-menu-right').append('<a class="dropdown-item">').val(sourceArray[i].url).text(sourceArray[i].name);
+        $('#dropdown-menu-left').append('<a class="dropdown-item">').val(sourceArray[i].url).text(sourceArray[i].name);
 
-}
+        $('#dropdown-menu-right').append('<a class="dropdown-item">').val(sourceArray[i].url).text(sourceArray[i].name);
+
+    }
 });
+
+// on-click function to make an API request and display the response data in html:
 
 $(document).on('click', '#searchBtn', function articleFunction(event) {
 
     event.preventDefault();
 
+    // placeholder values until dropdown is fixed:
+
     let source1 = 'nytimes.com';
     let source2 = 'foxnews.com';
-    let keyword = $('#searchBar').val();
-    console.log(keyword);
 
-    let query1 = 'https://newsapi.org/v2/everything?language=en&qinTitle=' + keyword + '&domains=' + source1 + '&pageSize=1&apiKey=' + APIkey + '';
-    let query2 = 'https://newsapi.org/v2/everything?language=en&qinTitle=' + keyword + '&domains=' + source2 + '&pageSize=1&apiKey=' + APIkey + '';
+    // keyword taken from user input:
+
+    let keyword = $('#searchBar').val();
+
+    // input validation checking for valid input:
+
+    if (keyword == '') {
+        alert('please enter a keyword')
+        return;
+    } else if (keyword.includes(' ')) {
+        alert('please enter only one word')
+        return;
+    } else if (keyword.length < 3) {
+        alert('please enter a longer keyword')
+        return;
+    } else {
+        console.log(keyword);
+    }
+
+    // urls for ajax query, dynamically generated with user input:
+
+    let query1 = 'https://newsapi.org/v2/everything?language=en&qinTitle=' + keyword + '&domains=' + source1 + '&pageSize=5&apiKey=' + APIkey + '';
+    let query2 = 'https://newsapi.org/v2/everything?language=en&qinTitle=' + keyword + '&domains=' + source2 + '&pageSize=5&apiKey=' + APIkey + '';
 
     $.ajax({
             url: query1,
@@ -68,29 +91,39 @@ $(document).on('click', '#searchBtn', function articleFunction(event) {
 
             for (let i = 0; i < response.articles.length; i++) {
 
+                // jQuery creating html elements from the response:
+
+                let articleDivLeft = $('<div class="jumbotron" id="articleDivLeft">');
                 let articleDiv = $("<div id='articleContent'" + i + ">");
                 let sourceName = $('<h3 id="sourceName">').text(source1);
                 let author = $('<h4 id="author">').text(response.articles[i].author);
                 let datePublished = $('<h4 id="author">').text(response.articles[i].publishedAt);
-
+                let wordCloudDiv = $('<div id="word-cloud">');
                 let title = $("<h3 id='title'>");
-                $(title).addClass("title");
-                $(title).text(response.articles[i].title);
-
                 let article = $("<p id='content'>");
 
+                // modifying the title and article elements:
+
+                $(title).addClass("title");
+                $(title).text(response.articles[i].title);
                 $(article).addClass("article");
                 $(article).text(response.articles[i].content);
+
+                // appending the elements to the articleDiv:
 
                 $(sourceName).appendTo(articleDiv);
                 $(title).appendTo(articleDiv);
                 $(author).appendTo(articleDiv);
                 $(datePublished).appendTo(articleDiv);
                 $(article).appendTo(articleDiv);
-                $(articleDiv).appendTo('#articleDivLeft');
+                $(wordCloudDiv).appendTo(articleDiv);
+
+                // appending the div to the html:
+
+                $(articleDiv).appendTo(articleDivLeft);
+                $(articleDivLeft).appendTo('#left');
 
             }
-
         });
 
     $.ajax({
@@ -104,34 +137,95 @@ $(document).on('click', '#searchBtn', function articleFunction(event) {
 
             for (let i = 0; i < response.articles.length; i++) {
 
+                // jQuery creating html elements from the response:
+
+                let articleDivRight = $('<div class="jumbotron" id="articleDivRight">');
                 let articleDiv = $("<div id='articleContent'" + i + ">");
                 let sourceName = $('<h3 id="sourceName">').text(source2);
                 let author = $('<h4 id="author">').text(response.articles[i].author);
                 let datePublished = $('<h4 id="author">').text(response.articles[i].publishedAt);
-
+                let wordCloudDiv = $('<div id="word-cloud">');
                 let title = $("<h3 id='title'>");
-                $(title).addClass("title");
-                $(title).text(response.articles[i].title);
-
                 let article = $("<p id='content'>");
 
+                // modifying the title and article elements:
+
+
+                $(title).addClass("title");
+                $(title).text(response.articles[i].title);
                 $(article).addClass("article");
                 $(article).text(response.articles[i].content);
+
+                // appending the elements to the articleDiv:
 
                 $(sourceName).appendTo(articleDiv);
                 $(title).appendTo(articleDiv);
                 $(author).appendTo(articleDiv);
                 $(datePublished).appendTo(articleDiv);
                 $(article).appendTo(articleDiv);
-                $(articleDiv).appendTo('#articleDivRight');
+                $(wordCloudDiv).appendTo(articleDiv);
+
+                // appending the div to the html:
+
+                $(articleDiv).appendTo(articleDivRight);
+                $(articleDivRight).appendTo('#right');
 
             }
+        });
+});
 
 
+// word cloud:
+
+let width = 300;
+let height = 300;
+//let fill = d3.scaleOrdinal(d3.schemeCategory20);
+let fill = d3.scale.category20();
 
 
+d3.layout.cloud().size([300, 300])
+    .words([
+        "I", "love", "you"
+    ].map(function (d) {
+        return {
+            text: d,
+            size: 10 + Math.random() * 90
+        };
+    }))
+    .padding(5)
+    .rotate(function () {
+        return ~~(
+            Math.random() * 2
+        ) * 90;
+    })
+    .font("Impact")
+    .fontSize(function (d) {
+        return d.size;
+    })
+    .on("end", draw)
+    .start();
 
+function draw(words) {
+    d3.select("#word-cloud").append("svg")
+        .attr("width", 300)
+        .attr("height", 300)
+        .append("g")
+        .attr("transform", "translate(" + (width / 2) + "," + (height / 2) + ")")
+        .selectAll("text")
+        .style("font-size", function (d) {
+            return d.size + "px";
+        })
+        .style("font-family", "Impact")
+        .style("fill", function (d, i) {
+            return fill(i);
+        })
+        .attr("text-anchor", "middle")
+        .attr("transform", function (d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+
+        .text(function (d) {
+            return d.text;
         });
 
-
-});
+}
