8bc37e823f11eada46d6464150edc835fddf6cd6
Will Hathaway <william.hathaway38@gmail.com>
2019-11-08 10:21:49 -0500

    word cloud is working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,9 +13,6 @@ let keyword = '';
 let source1 = '';
 let source2 = '';
 
-
-
-
 // an array of objects to hold the information for constructing the query:
 
 let sourceArray = [
@@ -158,10 +155,10 @@ $(document).on('click', '#searchBtn', function articleFunction(event) {
                     let sourceName = $('<a id="sourceName" href=' + source1 + '>');
                     let author = $('<h4 id="author">').text(response.articles[i].author);
                     let datePublished = $('<h4 id="author">').text(response.articles[i].publishedAt);
-                    let wordCloudDiv = $('<div class="child"id="word-cloud">');
                     let title = $("<a id='title' href=" + response.articles[i].url + ">");
                     let article = $("<p id='content'>");
-                    
+
+                    let wordCloudDiv = $('<div id="word-cloud-1">');
 
 
                     // modifying the title and article elements:
@@ -190,6 +187,9 @@ $(document).on('click', '#searchBtn', function articleFunction(event) {
                     $(articleContent).appendTo(articleDivLeft);
                     $(articleDivLeft).appendTo(target);
 
+                    cloudify(response.articles[i].content, "word-cloud-1", response.articles[i].title);
+
+
                 }
             });
 
@@ -212,9 +212,11 @@ $(document).on('click', '#searchBtn', function articleFunction(event) {
                     let sourceName = $('<a id="sourceName" href=' + source2 + '>');
                     let author = $('<h4 id="author">').text(response.articles[i].author);
                     let datePublished = $('<h4 id="author">').text(response.articles[i].publishedAt);
-                    let wordCloudDiv = $('<div class= "child" id="word-cloud">');
                     let title = $("<a id='title' href=" + response.articles[i].url + ">");
                     let article = $("<p id='content'>");
+
+                    let wordCloudDiv = $('<div id="word-cloud-2">');
+
 
                     // modifying the title and article elements:
 
@@ -224,6 +226,7 @@ $(document).on('click', '#searchBtn', function articleFunction(event) {
                     $(article).addClass("article");
                     $(article).text(response.articles[i].content);
                     $(logoImg).append(logo2);
+
 
                     // appending the elements to the articleDiv:
 
@@ -243,104 +246,45 @@ $(document).on('click', '#searchBtn', function articleFunction(event) {
                     $(articleContent).appendTo(articleDivRight);
                     $(articleDivRight).appendTo(target);
 
+                    cloudify(response.articles[i].content, "word-cloud-2", response.articles[i].title);
+
                 }
             });
     }
 });
 
 
-// word cloud:
 
-let text_string = "Sing in me, Muse, and through me tell the story of that man skilled in all ways of contending, the wanderer, harried for years on end, after he plundered the stronghold on the proud height of Troy. He saw the townlands and learned the minds of many distant men, and weathered many bitter nights and days in his deep heart at sea, while he fought only to save his life, to bring his shipmates home.";
+// var text =
+//     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum erat ac justo sollicitudin, quis lacinia ligula fringilla. Pellentesque hendrerit, nisi vitae posuere condimentum, lectus urna accumsan libero, rutrum commodo mi lacus pretium erat. Phasellus pretium ultrices mi sed semper. Praesent ut tristique magna. Donec nisl tellus, sagittis ut tempus sit amet, consectetur eget erat. Sed ornare gravida lacinia. Curabitur iaculis metus purus, eget pretium est laoreet ut. Quisque tristique augue ac eros malesuada, vitae facilisis mauris sollicitudin. Mauris ac molestie nulla, vitae facilisis quam. Curabitur placerat ornare sem, in mattis purus posuere eget. Praesent non condimentum odio. Nunc aliquet, odio nec auctor congue, sapien justo dictum massa, nec fermentum massa sapien non tellus. Praesent luctus eros et nunc pretium hendrerit. In consequat et eros nec interdum. Ut neque dui, maximus id elit ac, consequat pretium tellus. Nullam vel accumsan lorem.';
 
-drawWordCloud(text_string);
+function cloudify(text, location, title) {
 
-function drawWordCloud(text_string) {
-    var common = "poop,i,me,my,myself,we,us,our,ours,ourselves,you,your,yours,yourself,yourselves,he,him,his,himself,she,her,hers,herself,it,its,itself,they,them,their,theirs,themselves,what,which,who,whom,whose,this,that,these,those,am,is,are,was,were,be,been,being,have,has,had,having,do,does,did,doing,will,would,should,can,could,ought,i'm,you're,he's,she's,it's,we're,they're,i've,you've,we've,they've,i'd,you'd,he'd,she'd,we'd,they'd,i'll,you'll,he'll,she'll,we'll,they'll,isn't,aren't,wasn't,weren't,hasn't,haven't,hadn't,doesn't,don't,didn't,won't,wouldn't,shan't,shouldn't,can't,cannot,couldn't,mustn't,let's,that's,who's,what's,here's,there's,when's,where's,why's,how's,a,an,the,and,but,if,or,because,as,until,while,of,at,by,for,with,about,against,between,into,through,during,before,after,above,below,to,from,up,upon,down,in,out,on,off,over,under,again,further,then,once,here,there,when,where,why,how,all,any,both,each,few,more,most,other,some,such,no,nor,not,only,own,same,so,than,too,very,say,says,said,shall";
-
-    var word_count = {};
-
-    var words = text_string.split(/[ '\-\(\)\*":;\[\]|{},.!?]+/);
-    if (words.length == 1) {
-        word_count[words[0]] = 1;
-    } else {
-        words.forEach(function (word) {
-            var word = word.toLowerCase();
-            if (word != "" && common.indexOf(word) == -1 && word.length > 1) {
-                if (word_count[word]) {
-                    word_count[word]++;
-                } else {
-                  word_count[word] = 1;
-                }
-              }
-            })
-          }
-
-        var svg_location = "#chart";
-        var width = $('#word-cloud').width();
-        var height = $('#word-cloud').height();
-
-        var fill = d3.scale.category20();
-
-    var svg_location = "#chart";
-    var width = $('#word-cloud').width();
-    var height = $('#word-cloud').height();
-
-    var fill = d3.scale.category20();
-
-    var word_entries = d3.entries(word_count);
-
-    var xScale = d3.scale.linear()
-        .domain([0, d3.max(word_entries, function (d) {
-            return d.value;
-        })])
-        .range([10, 100]);
-
-    d3.layout.cloud().size([width, height])
-        .timeInterval(20)
-        .words(word_entries)
-        .fontSize(function (d) {
-            return xScale(+d.value);
-        })
-        .text(function (d) {
-            return d.key;
-        })
-        .rotate(function () {
-            return ~~(Math.random() * 2) * 90;
-        })
-        .font("Impact")
-        .on("end", draw)
-        .start();
-
-    function draw(words) {
-        d3.select(svg_location).append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append("g")
-              .attr("transform", "translate(" + [width >> 1, height >> 1] + ")")
-            .selectAll("text")
-              .data(words)
-            .enter().append("text")
-            .style("font-size", function (d) {
-                return xScale(d.value) + "px";
-            })
-            .style("font-family", "Impact")
-            .style("fill", function (d, i) {
-                return fill(i);
-            })
-            .attr("text-anchor", "middle")
-            .attr("transform", function (d) {
-                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-            })
-            .text(function (d) {
-                return d.key;
+    var lines = text.split(/[,\. ]+/g),
+        data = Highcharts.reduce(lines, function (arr, word) {
+            var obj = Highcharts.find(arr, function (obj) {
+                return obj.name === word;
             });
-    }
+            if (obj) {
+                obj.weight += 1;
+            } else {
+                obj = {
+                    name: word,
+                    weight: 1
+                };
+                arr.push(obj);
+            }
+            return arr;
+        }, []);
 
-    d3.layout.cloud().stop();
-}
-
-let wordCloudTest = $('<div>');
-wordCloudTest.append(drawWordCloud(text_string));
-$('#meow').append(wordCloudTest);
-
+    Highcharts.chart(location, {
+        series: [{
+            type: 'wordcloud',
+            data: data,
+            name: 'Occurrences'
+        }],
+        title: {
+            text: 'Wordcloud of ' + title + ''
+        }
+    })
+};
