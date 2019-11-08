111386c29a792a9b55880b521049e19af7a58009
willhathaway <53982876+willhathaway@users.noreply.github.com>
2019-11-08 11:00:10 -0500

    Final day branch (#19)
    * working on search functionality
    
    * search done
    
    * added code/js file for d3 word cloud
    
    * query working. sources dropdown not working yet.
    
    * sources dropdown working
    
    * styling and modifying DOM divs
    
    * logos now appear above the articles
    
    * .
    
    * query complete. d3 word cloud in progress
    
    * Before Pull
    
    * word cloud is working
    
    * deleted cat div
    
    * fixed cloud bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -158,7 +158,10 @@ $(document).on('click', '#searchBtn', function articleFunction(event) {
                     let title = $("<a id='title' href=" + response.articles[i].url + ">");
                     let article = $("<p id='content'>");
 
-                    let wordCloudDiv = $('<div id="word-cloud-1">');
+
+                    let wordCloudDiv = $('<div id="word-cloud-left' + i + '" >');
+                    let cloudID = wordCloudDiv.attr('id');
+
 
 
                     // modifying the title and article elements:
@@ -187,7 +190,9 @@ $(document).on('click', '#searchBtn', function articleFunction(event) {
                     $(articleContent).appendTo(articleDivLeft);
                     $(articleDivLeft).appendTo(target);
 
-                    cloudify(response.articles[i].content, "word-cloud-1", response.articles[i].title);
+
+                    cloudify(response.articles[i].content, cloudID, response.articles[i].title);
+
 
 
                 }
@@ -215,7 +220,9 @@ $(document).on('click', '#searchBtn', function articleFunction(event) {
                     let title = $("<a id='title' href=" + response.articles[i].url + ">");
                     let article = $("<p id='content'>");
 
-                    let wordCloudDiv = $('<div id="word-cloud-2">');
+
+                    let wordCloudDiv = $('<div id="word-cloud-right' + i + '" >');
+                    let cloudID = wordCloudDiv.attr('id');
 
 
                     // modifying the title and article elements:
@@ -246,7 +253,9 @@ $(document).on('click', '#searchBtn', function articleFunction(event) {
                     $(articleContent).appendTo(articleDivRight);
                     $(articleDivRight).appendTo(target);
 
-                    cloudify(response.articles[i].content, "word-cloud-2", response.articles[i].title);
+
+                    cloudify(response.articles[i].content, cloudID, response.articles[i].title);
+
 
                 }
             });
